6e4fe6dbbfa243e0502c266cde579f6964c3bcfe
edusatoe <edu.satoe@gmail.com>
2019-11-29 14:53:19 +0100

    Tech-inicial

--- src/allintelligence/shodanwrapper.py
+++ src/allintelligence/shodanwrapper.py
@@ -1,25 +1,41 @@
 import shodan
-from allintelligence.config import SHODAN_API_KEY
+from config import SHODAN_API_KEY
+"""
+Módulo de interconexión con la API de Shodan para obtener información de shodan a parti de una IP
+
+__author__:AllPentesting
+"""
  
 def gethost(ip):
+    """
+    Función que concecta con la API Shodan y nos devuelve una serie de información en un diccionario
+    Parametros:
+        - ip: dirección ip del dominio a consultar
+
+    La información que devueleve es: ASN, ISP, City, Latitude, Longitude, Country, Organization, Array de Hostnames,Array de ports,Array de vulns y Arrays de datos
+    como información del CVE, CVSS, si está verificado o información y enlaces de la vulnerabilidad
+
+    """
     api = shodan.Shodan(SHODAN_API_KEY)
 
     host_info = api.host(ip)
-    '''
-        host_info['asn'] -> ASN
-        host_info['city'] -> Puede ser None
-        host_info['hostnames'] -> array
-        host_info['isp']
-        host_info['org']
-        host_info['ports'] -> Puertos abiertos array
-        host_info['vulns'] -> Array
-    '''
+    #print (host_info['vulns'])
 
     return {
         'ip': host_info['ip_str'],
-        'ans': host_info['asn'],
-        'hostnames': host_info['hostanames'],
+        'asn': host_info['asn'],
+        'isp': host_info['isp'],
+        'city': host_info['city'],
+        'latitude': host_info['latitude'],
+        'longitude': host_info['longitude'],
+        'country_name': host_info['country_name'],
         'org': host_info['org'],
+        'last_update': host_info['last_update'],  
+        'hostnames': host_info['hostnames'],
         'ports': host_info['ports'],
-        'vulns': host_info['vulns']
+        'vulns': host_info['vulns'],
+        'data': host_info['data']
+    
     }
+
+gethost("150.214.110.200")
--- src/allintelligence/tech.py
+++ src/allintelligence/tech.py
@@ -1,4 +1,54 @@
+import socket
+import requests
+import dns.resolver
+import shodanwrapper
+
 def analyze(domain):
     global_dictionary = {}
-    return global_dictionary
+
+    #Comprobamos si el dominio resuelve DNS o no
+    try:
+        dns_results= dns.resolver.query(domain)
+        dns_records = [ip.address for ip in dns_results]
+        domainOn= True
+    except:
+        domainOn= False
+        
     
+    #Obtención de la IP del dominio
+    if domainOn:
+        ip=socket.gethostbyname(str(domain)) 
+
+
+    #Módulo de whois y contractar manualmente
+
+
+        #Módulo de shodan 
+        dict_shodan = shodanwrapper.gethost(ip)
+        print (dict_shodan)
+
+
+   
+
+    #Módulo de maxmind 
+
+
+
+    print(domainOn)
+    if domainOn:
+        print(ip)
+    return global_dictionary
+
+
+def analyze_SMTP(domai):
+    pass
+
+
+def analyze_HTTP_S(domai):
+    pass
+
+
+
+#domain="allpentesting.es"
+domain="lucano.uco.es"
+analyze(domain)

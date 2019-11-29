cd328a0d4d3da9017a4fdf1581793f11a0de52f4
edusatoe <edu.satoe@gmail.com>
2019-11-29 15:28:51 +0100

    Revisión de keys en Shodan

--- src/allintelligence/shodanwrapper.py
+++ src/allintelligence/shodanwrapper.py
@@ -19,23 +19,78 @@ def gethost(ip):
     api = shodan.Shodan(SHODAN_API_KEY)
 
     host_info = api.host(ip)
-    #print (host_info['vulns'])
-
-    return {
-        'ip': host_info['ip_str'],
-        'asn': host_info['asn'],
-        'isp': host_info['isp'],
-        'city': host_info['city'],
-        'latitude': host_info['latitude'],
-        'longitude': host_info['longitude'],
-        'country_name': host_info['country_name'],
-        'org': host_info['org'],
-        'last_update': host_info['last_update'],  
-        'hostnames': host_info['hostnames'],
-        'ports': host_info['ports'],
-        'vulns': host_info['vulns'],
-        'data': host_info['data']
     
-    }
+        
+    #Diccionario principal con toda la información de shodan
+    dict_shodan = {}
 
-gethost("150.214.110.200")
+    #Comprobamos si existe cada una de las keys y sino le asignamos None para que no de fallo en caso de acceso 
+    if "ip_str" in host_info:
+        dict_shodan.update({"ip":host_info['ip_str']})
+    else:
+        dict_shodan.update({"ip": None})
+    
+
+    if "asn" in host_info:
+        dict_shodan.update({"asn":host_info['asn']})
+    else:
+        dict_shodan.update({"asn": None})
+
+    if "isp" in host_info:
+        dict_shodan.update({"isp":host_info['isp']})
+    else:
+        dict_shodan.update({"isp": None})
+
+    if "city" in host_info:
+        dict_shodan.update({"city":host_info['city']})
+    else:
+        dict_shodan.update({"city": None})
+
+    if "latitude" in host_info:
+        dict_shodan.update({"latitude":host_info['latitude']})
+    else:
+        dict_shodan.update({"latitude": None})
+
+    if "longitude" in host_info:
+        dict_shodan.update({"longitude":host_info['longitude']})
+    else:
+        dict_shodan.update({"longitude": None})
+
+    if "country_name" in host_info:
+        dict_shodan.update({"country_name":host_info['country_name']})
+    else:
+        dict_shodan.update({"country_name": None})
+
+    if "org" in host_info:
+        dict_shodan.update({"org":host_info['org']})
+    else:
+        dict_shodan.update({"org": None})
+
+    if "last_update" in host_info:
+        dict_shodan.update({"last_update":host_info['last_update']})
+    else:
+        dict_shodan.update({"last_update": None})
+    
+    if "hostnames" in host_info:
+        dict_shodan.update({"hostnames":host_info['hostnames']})
+    else:
+        dict_shodan.update({"hostnames": None})
+
+    if "ports" in host_info:
+        dict_shodan.update({"ports":host_info['ports']})
+    else:
+        dict_shodan.update({"ports": None})
+
+    if "vulns" in host_info:
+        dict_shodan.update({"vulns":host_info['vulns']})
+    else:
+        dict_shodan.update({"vulns": None})
+
+    if "data" in host_info:
+        dict_shodan.update({"data":host_info['data']})
+    else:
+        dict_shodan.update({"data": None})
+
+    return dict_shodan
+
+
